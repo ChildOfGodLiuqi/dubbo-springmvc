e94502760dec55034cbabb837055ead53b61ae03
还是相信爱过 <191287278@qq.com>
2016-03-21 12:51:04 +0800

    更新样例

--- dubbo-springmvc-example/src/main/java/com/vcg/example/service/impl/UserServiceImpl.java
+++ dubbo-springmvc-example/src/main/java/com/vcg/example/service/impl/UserServiceImpl.java
@@ -16,19 +16,19 @@ import com.vcg.example.service.UserService;
 public class UserServiceImpl implements UserService {
 
 	@Override
-	// http://localhost:8090/defaultGroup/1.0.0/json/userService/getById?id=1
+	// http://localhost:8090/defaultGroup/0.0.0/userService/getById?id=1
 	public User getById(Integer id) {
 		return new User(id, "test1", "123456", new Date());
 	}
 
 	@Override
-	// http://localhost:8090/defaultGroup/1.0.0/json/userService/deleteById?id=1
+	// http://localhost:8090/defaultGroup/0.0.0/userService/deleteById?id=1
 	public void deleteById(Integer id) {
 		System.out.println("删除用户 :" + id);
 	}
 
 	@Override
-	// http://localhost:8090/defaultGroup/1.0.0/json/userService/deleteById?id=1&username=wuyu&password=1
+	// http://localhost:8090/defaultGroup/0.0.0/userService/deleteById?id=1&username=wuyu&password=1
 	public Integer insert(User user) {
 		System.out.println("插入用户" + user.toString());
 		user.setId(1);
@@ -36,7 +36,7 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	//http://localhost:8090/defaultGroup/1.0.0/json/userService/testException
+	//http://localhost:8090/defaultGroup/0.0.0/userService/testException
 	public void testException(Integer id) {
 		if (id == null || id.equals("testException")) {
 			throw new RuntimeException("未找到相关用户");
@@ -44,7 +44,7 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	//http://localhost:8090/defaultGroup/1.0.0/json/userService/testErrorMsgException
+	//http://localhost:8090/defaultGroup/0.0.0/userService/testErrorMsgException
 	@ErrorMsg(msg = "测试自定义测试信息", responseType = "text/xml;charset=utf-8")
 	public void testErrorMsgException(Integer id) {
 		if (id == null || id.equals("testException")) {
