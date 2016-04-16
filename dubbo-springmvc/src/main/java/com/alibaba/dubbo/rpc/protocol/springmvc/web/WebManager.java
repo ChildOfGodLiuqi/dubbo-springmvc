69cc741efe43a0228bf9bfdd9285c9d25cb411a7
还是相信爱过 <191287278@qq.com>
2016-04-16 23:05:01 +0800

    add monitor

--- dubbo-springmvc/src/main/java/com/alibaba/dubbo/rpc/protocol/springmvc/web/WebManager.java
+++ dubbo-springmvc/src/main/java/com/alibaba/dubbo/rpc/protocol/springmvc/web/WebManager.java
@@ -1,23 +1,39 @@
 package com.alibaba.dubbo.rpc.protocol.springmvc.web;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils.MethodFilter;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.HandlerMethodSelector;
 
-import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.utils.NetUtils;
-import com.alibaba.dubbo.rpc.RpcContext;
+import com.alibaba.dubbo.rpc.protocol.springmvc.util.SpringUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 public class WebManager {
 
@@ -26,22 +42,230 @@ public class WebManager {
 	private static String template = "<tr class=\"%s\"><td>%s</td><td>%s</td></td><td><a href=\"%s\">%s</td><td><button onclick='invokerPop(\"%s\")' class=\"btn btn-default btn-primary btn-sm\">调用</button></td></tr>";
 	private static List<String> cssTrClass = Arrays.asList("", "info");
 
-	private Map<Object, HashSet<String>> urls = new ConcurrentHashMap<Object, HashSet<String>>();
+	private Map<Object, HashSet<String>> handlerMappings = new ConcurrentHashMap<Object, HashSet<String>>();
 
 	@RequestMapping("/services")
 	public void services(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		int serverPort = request.getServerPort();
 		String hostAddress = NetUtils.getLocalAddress().getHostAddress();
 		String addr = request.getScheme() + "://" + hostAddress + ":" + serverPort;
-		String genHtml = WebManager.genHtml(urls, addr);
+		String genHtml = WebManager.genHtml(handlerMappings, addr);
 		response.setContentType("text/html;charset=utf-8");
 		response.getWriter().write(genHtml);
 		response.flushBuffer();
 	}
 
+	@RequestMapping("/servicesJ")
+	@ResponseBody
+	public JSONObject servicesJ(HttpServletRequest request) {
+
+		int serverPort = request.getServerPort();
+		String hostAddress = NetUtils.getLocalAddress().getHostAddress();
+		String addr = request.getScheme() + "://" + hostAddress + ":" + serverPort;
+
+		JSONObject handlerJson = new JSONObject();
+
+		for (Object handler : handlerMappings.keySet()) {
+			Class<?> userClass = ClassUtils.getUserClass(handler);
+			String handlerName = userClass.getName();
+			JSONObject jsonObject = new JSONObject();
+			handlerJson.put(handlerName, jsonObject);
+			ArrayList<String> paths = new ArrayList<String>(handlerMappings.get(handler));
+			Collections.sort(paths);
+			Method[] methods = userClass.getMethods();
+			for (Method method : methods) {
+				for (String path : paths) {
+					String[] split = path.split("/");
+					String methodName = split[split.length - 1];
+					if (method.getName().equals(methodName)) {
+						jsonObject.put(method.toString(), addr + path);
+					}
+				}
+			}
+
+		}
+		return handlerJson;
+	}
+
+	@RequestMapping("/beans")
+	@ResponseBody
+	public JSONObject beans() {
+		JSONObject jsonObject = new JSONObject();
+		Set<ApplicationContext> applicationContexts = SpringUtil.getApplicationContexts();
+		for (ApplicationContext applicationContext : applicationContexts) {
+			String[] beanNames = applicationContext.getBeanDefinitionNames();
+			for (String beanName : beanNames) {
+				Object bean = applicationContext.getBean(beanName);
+				Set<Method> selectMethods = HandlerMethodSelector.selectMethods(bean.getClass(), new MethodFilter() {
+
+					public boolean matches(Method method) {
+						return true;
+					}
+				});
+
+				List<String> methods = new ArrayList<String>();
+				for (Method method : selectMethods) {
+					methods.add(method.toString());
+				}
+				jsonObject.put(ClassUtils.getUserClass(bean).getName(), methods);
+			}
+		}
+		return jsonObject;
+	}
+
+	@RequestMapping("/dataSource")
+	@ResponseBody
+	public Object dataSource() throws SQLException {
+		Set<String> dataSourceNames = SpringUtil.getBeanNamesForType(DataSource.class);
+		JSONObject jsonObject = new JSONObject();
+		JSONObject dataSourceInfo = new JSONObject();
+		for (String dataSourceName : dataSourceNames) {
+			JSONObject dataSourceJson = new JSONObject();
+			DataSource dataSource = SpringUtil.getBean(dataSourceName);
+
+			Connection conn = dataSource.getConnection();
+			if (conn == null) {
+				continue;
+			}
+			DatabaseMetaData metaData = conn.getMetaData();
+			String username = metaData.getUserName();
+			String url = metaData.getURL();
+			String driverName = metaData.getDriverVersion();
+			dataSourceInfo.put("url", url);
+			dataSourceInfo.put("username", username);
+			dataSourceInfo.put("driverName", driverName);
+			ResultSet resultSet = metaData.getTables(null, null, null, null);
+			JSONObject tables = new JSONObject();
+			while (resultSet.next()) {
+				String tableName = resultSet.getString("TABLE_NAME");
+				ResultSet colRet = metaData.getColumns(null, null, tableName, null);
+				JSONObject columnJson = new JSONObject();
+				tables.put(tableName, columnJson);
+				while (colRet.next()) {
+					JSONObject columnInfo = new JSONObject();
+					String columnName = colRet.getString("COLUMN_NAME");
+					// 字段类型
+					String columnType = colRet.getString("TYPE_NAME");
+					// 字段长度
+					int datasize = colRet.getInt("COLUMN_SIZE");
+					// 注释
+					String remarks = colRet.getString("REMARKS");
+					// DECIMAL精度
+					int digits = colRet.getInt("DECIMAL_DIGITS");
+					columnInfo.put("columnType", columnType);
+					columnInfo.put("columnSize", datasize);
+					columnInfo.put("digits", digits);
+					columnInfo.put("remarks", remarks);
+					columnJson.put(columnName, columnInfo);
+				}
+			}
+			conn.close();
+			dataSourceJson.put("tables", tables);
+			dataSourceJson.put("dataSourceInfo", dataSourceInfo);
+			jsonObject.put(dataSourceName, dataSourceJson);
+
+			Field[] fields = dataSource.getClass().getDeclaredFields();
+			for (Field field : fields) {
+				try {
+					Class<?> type = field.getType();
+					if (isNumber(type)) {
+
+						field.setAccessible(true);
+						dataSourceInfo.put(field.getName(), field.get(dataSource));
+					}
+				} catch (Exception e) {
+				}
+			}
+
+			Method[] methods = dataSource.getClass().getMethods();
+			for (Method method : methods) {
+				try {
+					String methodName = method.getName();
+					if (methodName.equals("getMaxActive")) {
+						dataSourceInfo.put("maxActive", method.invoke(dataSource));
+					}
+					if (methodName.equals("getMaxIdle")) {
+						dataSourceInfo.put("maxIdle", method.invoke(dataSource));
+					}
+
+					if (methodName.equals("getMinIdle")) {
+						dataSourceInfo.put("minIdle", method.invoke(dataSource));
+					}
+
+					if (methodName.equals("getMaxWait")) {
+						dataSourceInfo.put("maxWait", method.invoke(dataSource));
+					}
+
+					if (methodName.equals("getQueryTimeout")) {
+						dataSourceInfo.put("queryTimeout", method.invoke(dataSource));
+					}
+					if (methodName.equals("getExecuteCount")) {
+						dataSourceInfo.put("executeCount", method.invoke(dataSource));
+					}
+					if (methodName.equals("getInitialSize")) {
+						dataSourceInfo.put("initialSize", method.invoke(dataSource));
+					}
+					if (methodName.equals("getTimeBetweenConnectErrorMillis")) {
+						dataSourceInfo.put("timeBetweenConnectErrorMillis", method.invoke(dataSource));
+					}
+					if (methodName.equals("getTimeBetweenEvictionRunsMillis()")) {
+						dataSourceInfo.put("timeBetweenEvictionRunsMillis", method.invoke(dataSource));
+					}
+					if (methodName.equals("getMinEvictableIdleTimeMillis()")) {
+						dataSourceInfo.put("minEvictableIdleTimeMillis", method.invoke(dataSource));
+					}
+
+				} catch (Exception e) {
+				}
+
+			}
+
+		}
+		return jsonObject;
+
+	}
+
+	@RequestMapping("/env")
+	@ResponseBody
+	public Object env() {
+		return System.getProperties();
+	}
+
+	@RequestMapping("/mem")
+	@ResponseBody
+	public JSONObject mem() {
+		JSONObject jsonObject = new JSONObject();
+		Runtime r = Runtime.getRuntime();
+		jsonObject.put("jvmTotal", r.totalMemory());// java总内存
+		jsonObject.put("jvmUse", r.totalMemory() - r.freeMemory());// JVM使用内存
+		jsonObject.put("jvmFree", r.freeMemory());// JVM剩余内存
+		BigDecimal divide = new BigDecimal((r.totalMemory() - r.freeMemory())).divide(new BigDecimal(r.totalMemory()),
+				2, 4);
+		jsonObject.put("jvmUsage", divide.doubleValue());// JVM使用率
+		jsonObject.put("cpu", r.availableProcessors());
+		return jsonObject;
+	}
+
+	@RequestMapping("/thread")
+	@ResponseBody
+	public Object thread() {
+		JSONArray jsonArray = new JSONArray();
+		long[] allThreadIds = ManagementFactory.getThreadMXBean().getAllThreadIds();
+		for (long l : allThreadIds) {
+			ThreadInfo threadInfo = ManagementFactory.getThreadMXBean().getThreadInfo(l);
+			jsonArray.add(JSON.parseObject(JSON.toJSONString(threadInfo)));
+		}
+		return jsonArray;
+	}
+
+	public boolean isNumber(Class clazz) {
+		return clazz.isAssignableFrom(Integer.class) || clazz.isAssignableFrom(Long.class)
+				|| clazz.getName().equalsIgnoreCase("INT") || clazz.getName().equalsIgnoreCase("LONG");
+	}
+
 	public WebManager(Map<Object, HashSet<String>> urls) {
 		super();
-		this.urls = urls;
+		this.handlerMappings = urls;
 	}
 
 	public static String genHtml(Map<Object, HashSet<String>> mappingds, String addr) {
@@ -72,7 +296,7 @@ public class WebManager {
 	}
 
 	public void setUrls(Map<Object, HashSet<String>> urls) {
-		this.urls = urls;
+		this.handlerMappings = urls;
 	}
 
 	private static String invokerPop = "<div class='modal' id='mymodal'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'><span aria-hidden='true'>×</span><span class='sr-only'>Close</span></button><h4 class='modal-title'>调用方法,url注意跨域!</h4></div><div class='modal-body'><form role='form'><div class='form-group'><label for='invokerUrl'>地址：</label> <input type='url'class='form-control' id='invokerUrl' value='' placeholder='调用地址'></div><div class='form-group'><label for='invokerMethod'>参数:</label> <input type='text'class='form-control' value='' id='invokerArgs'placeholder='参数以json格式传递:{\"name\":\"wuyu\"},如果没有参数:{}'></div><div class='form-group'><label for='invokerMethod'>次数:</label> <input type='number'class='form-control' value='1' id='invokerCount'placeholder='調用次数'></div><div class='form-group'><label for='invokerMethod'> 请求头:</label></br> <label class='radio-inline'> <input type='radio' name='reqeustHeader' checked='checked' id='inlineRadio1' value='application/x-www-form-urlencoded; charset=UTF-8'> application/x-www-form-urlencoded</label> <label class='radio-inline'> <input type='radio' name='reqeustHeader' id='inlineRadio2' value='application/json;charset=UTF-8'>application/json</div><div class='form-group'><label for='invokerMethod'>result:</label><div id='result'></div></div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>关闭</button><button type='button' id='execute' onclick='invoker()' class='btn btn-primary'>执行</button></div></form></div></div></div></div>";
