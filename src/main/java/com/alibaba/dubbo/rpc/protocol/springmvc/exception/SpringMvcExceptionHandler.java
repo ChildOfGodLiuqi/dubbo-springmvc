5a2adfaf4d6790eb0d661dda997bcdfe0ac7551f
还是相信爱过 <191287278@qq.com>
2016-09-15 14:12:24 +0800

    readme

--- src/main/java/com/alibaba/dubbo/rpc/protocol/springmvc/exception/SpringMvcExceptionHandler.java
+++ src/main/java/com/alibaba/dubbo/rpc/protocol/springmvc/exception/SpringMvcExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.alibaba.dubbo.rpc.protocol.springmvc.exception;
 
-import com.alibaba.fastjson.support.spring.FastJsonJsonView;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 /**
@@ -15,9 +16,15 @@ public class SpringMvcExceptionHandler implements HandlerExceptionResolver {
 
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        FastJsonJsonView view = new FastJsonJsonView();
-        view.addStaticAttribute("message", ex.toString());
-        view.addStaticAttribute("status", response.getStatus());
-        return new ModelAndView(view);
+        JSONObject view = new JSONObject();
+        view.put("message", ex.toString());
+        view.put("status",500);
+        try {
+            response.setContentType("application/json;charset=utf-8");
+            response.getWriter().write(view.toJSONString());
+        } catch (IOException e) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
